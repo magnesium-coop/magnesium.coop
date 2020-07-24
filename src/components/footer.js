7a26884b09b88484a61bd16b4de21b351022e702
raúl <rsperoni@magnesium.coop>
2020-07-24 16:52:44 -0300

    footer arrow

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { IoIosArrowUp } from "react-icons/io"
+import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io"
 
 const Footer = (props) => {
 
@@ -11,7 +11,9 @@ const Footer = (props) => {
     <footer className={"flex absolute bottom-0 right-0 w-56 z-40 mr-24 text-l bg-naranja"} id="footer-principal">
       <div className={"flex h-10 w-full"}>
         <a href={"#"} className={"flex w-full h-full items-center justify-center text-white font-mgbook no-underline"} onClick={() => toggleExpansion(!isExpanded)}>
-          <span>Contacto</span><span className={"w-3"}></span><span className={"text-xl"}><IoIosArrowUp/></span>
+          <span>Contacto</span><span className={"w-3"}></span>
+          <span className={`${isExpanded ? `block` : `hidden`} text-xl`}><IoIosArrowDown/></span>
+          <span className={`${isExpanded ? `hidden` : `block`} text-xl`}><IoIosArrowUp/></span>
         </a>
 
       </div>
