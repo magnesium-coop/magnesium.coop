42a7927100cf93201363b7755ede0673137bf38d
raúl <rsperoni@magnesium.coop>
2020-07-29 08:02:53 -0300

    estilos

--- src/components/about.js
+++ src/components/about.js
@@ -11,41 +11,49 @@ const AboutPage = (props) => {
   const slides = props.pages[props.pagePos].slides
   return (
     <Layout anchor={props.pages[props.pagePos].anchor} backgroundColor={props.currentPage.backgroundColor}>
-        <Slide backgroundColor={slides[0].backgroundColor}
-               textColor={slides[0].textColor}
-               slideAnchor={slides[0].anchor}
-               nextSlide={slides[1]}>
-          <h2 className={slides[0].titleColor}>{slides[0].title}</h2>
-          <div
-            className="blog-post-content"
-            dangerouslySetInnerHTML={{ __html: slides[0].html }}
-          />
-        </Slide>
-        <Slide backgroundColor={slides[1].backgroundColor}
-               textColor={slides[1].textColor}
-               slideAnchor={slides[1].anchor}
-               prevSlide={slides[0]}
-               nextSlide={slides[2]}>
-          <h1 className={slides[1].titleColor}>{slides[1].title}</h1>
-          {slides[1].autores.map((author) => (
-            <Bio author={author} key={author.id}/>
-          ))}
-        </Slide>
+      <Slide backgroundColor={slides[0].backgroundColor}
+             textColor={slides[0].textColor}
+             slideAnchor={slides[0].anchor}
+             nextSlide={slides[1]}>
+        <div className="flex">
+          <div className="w-0 md:w-1/6 lg:w-1/3"/>
+          <div>
+            <div className={"font-mgblack text-2xl mg:text-3xl lg:text-4xl "+slides[0].titleColor}>{slides[0].title}</div>
+            <div
+              className="text-justify text-lg md:text-xl lg:text-2xl"
+              dangerouslySetInnerHTML={{ __html: slides[0].html }}
+            />
+          </div>
+          <div className="w-0 md:w-1/6 lg:w-1/3"/>
 
-        <Slide backgroundColor={slides[2].backgroundColor}
-               textColor={slides[2].textColor}
-               slideAnchor={slides[2].anchor}
-               prevSlide={slides[1]}>
-          <h1 className={slides[2].titleColor}>{slides[2].title}</h1>
-          {slides[2].autores.map((author) => (
-            <Bio author={author} key={author.id}/>
-          ))}
-        </Slide>
+        </div>
 
-        <ArrowNext arrow={props.currentPage.next} fullPageApi={props.fullPageApi}
-                   anchor={props.pages[props.pagePos].anchor}/>
-        <ArrowPrev arrow={props.currentPage.prev} fullPageApi={props.fullPageApi}
-                   anchor={props.pages[props.pagePos].anchor}/>
+      </Slide>
+      <Slide backgroundColor={slides[1].backgroundColor}
+             textColor={slides[1].textColor}
+             slideAnchor={slides[1].anchor}
+             prevSlide={slides[0]}
+             nextSlide={slides[2]}>
+        <div className={"font-mgblack text-2xl mg:text-3xl lg:text-4xl "+slides[1].titleColor}>{slides[1].title}</div>
+        {slides[1].autores.map((author) => (
+          <Bio author={author} key={author.id}/>
+        ))}
+      </Slide>
+
+      <Slide backgroundColor={slides[2].backgroundColor}
+             textColor={slides[2].textColor}
+             slideAnchor={slides[2].anchor}
+             prevSlide={slides[1]}>
+        <div className={"font-mgblack text-2xl mg:text-3xl lg:text-4xl "+slides[2].titleColor}>{slides[2].title}</div>
+        {slides[2].autores.map((author) => (
+          <Bio author={author} key={author.id}/>
+        ))}
+      </Slide>
+
+      <ArrowNext arrow={props.currentPage.next} fullPageApi={props.fullPageApi}
+                 anchor={props.pages[props.pagePos].anchor}/>
+      <ArrowPrev arrow={props.currentPage.prev} fullPageApi={props.fullPageApi}
+                 anchor={props.pages[props.pagePos].anchor}/>
     </Layout>
   )
 }
