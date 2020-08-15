9f81ab6a7d4a9e37d0f5f235b6fd9a9582b5f8cc
raúl <rsperoni@magnesium.coop>
2020-08-15 15:57:25 -0300

    menu secundario

--- src/components/secondMenu.js
+++ src/components/secondMenu.js
@@ -5,46 +5,43 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React, { useState } from "react"
-import logomg01 from "../../content/assets/logo-mg-01.svg"
+import React from "react"
 
 const SecondMenu = ({ currentPage, currentSlide }) => {
 
 
   function getMenuItem(anchor, title) {
-    const classActive = anchor === currentSlide.anchor ? 'active' : '';
+    const classActive = anchor === currentSlide.anchor ? "active" : ""
     if (title) {
       return (
-        <li key={"menu-" + anchor} className={"nav-item flex justify-end lg:justify-start "+classActive} data-menuanchor={anchor}>
-          <a className={"self-end lg:px-3 py-1 no-underline leading-snug uppercase hover:opacity-75 " + currentSlide.textColor}
-             href={"#" + anchor}>
+        <li key={"menu-" + anchor} className={"nav-item flex " + classActive}
+            data-menuanchor={anchor}>
+          <a
+            className={"self-end lg:px-3 py-1 no-underline leading-snug uppercase hover:opacity-75 " + currentSlide.textColor}
+            href={"#" + anchor}>
             {title}
           </a>
         </li>
       )
     } else {
-      return null;
+      return null
     }
   }
 
   return (
-    <header className={"fixed bottom-0 z-40 w-screen font-mgbook text-l "} id="menu-secundario">
-      <nav
-        className={"w-auto flex justify-between lg:flex-col px-5 py-4"}>
-
-        <div className="lg:flex lg:mt-5 text-xs">
-          <ul id="ul-menu-secundario" className="flex list-none pl-0">
-            {
-              currentPage.slides.map(({ anchor, title }) => (
-                getMenuItem(anchor, title)
-              ))
-            }
-          </ul>
-          <div>
-          </div>
+    <footer className={"fixed bottom-0 z-40 w-screen font-mgbook"} id="menu-secundario">
+      <nav className={"w-auto flex  px-5 py-4"}>
+        <ul id="ul-menu-secundario" className="w-full md:w-50 flex text-xs list-none justify-around md:justify-start pl-0">
+          {
+            currentPage.slides.map(({ anchor, title }) => (
+              getMenuItem(anchor, title)
+            ))
+          }
+        </ul>
+        <div>
         </div>
       </nav>
-    </header>
+    </footer>
   )
 }
 
