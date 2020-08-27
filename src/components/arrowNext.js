4632e49b7dcdd5d08dc6a515bef587ce207d1620
raúl <rsperoni@magnesium.coop>
2020-08-27 20:30:30 -0300

    flechas mobile

--- src/components/arrowNext.js
+++ src/components/arrowNext.js
@@ -4,23 +4,24 @@ import { IoIosArrowForward } from "react-icons/io"
 const ArrowNext = ({ arrow, anchor, fullPageApi }) => {
   if (arrow !== undefined && arrow !== null) {
     return (
-      <div className={"flex items-center customArrow customArrowNext mt-32 mb-auto md:mt-auto lg:my-auto w-8 h-16 md:w-10 md:h-20 lg:w-24 lg:h-48 " + arrow.backgroundColor}>
-        <a href={"#" + anchor + "/" + arrow.anchor} className={"mx-auto no-underline text-3xl md:text-4xl lg:text-5xl " + arrow.textColor}
+      <div
+        className={"flex items-center customArrow customArrowNext my-auto w-8 h-40 md:w-10 md:h-20 lg:w-24 lg:h-48 " + arrow.backgroundColor}>
+        <a href={"#" + anchor + "/" + arrow.anchor}
+           className={"h-full w-full no-underline text-3xl md:text-4xl lg:text-5xl " + arrow.textColor}
            onClick={(e) => {
              e.preventDefault()
              console.log(anchor, arrow.anchor)
              fullPageApi.moveTo(anchor, arrow.anchor)
            }}>
-          <div className="flex justify-center items-center">
-            <span><IoIosArrowForward/></span>
-            <div>
-              <div className="invisible lg:visible relative h-20 w-10 min-w-full">
-                <span
-                  className={"arrowText rotateArrowTextNext text-sm whitespace-no-wrap uppercase " + arrow.titleColor}>{arrow.title}</span>
-              </div>
-
-            </div>
+          <div className="h-full w-full flex flex-wrap lg:flex-no-wrap justify-around align-center content-center">
+            <span className="mx-auto my-auto">
+              <IoIosArrowForward/>
+            </span>
+            <span style={{writingMode: "sideways-lr"}}
+              className={"mx-auto my-auto text-sm whitespace-no-wrap uppercase " + arrow.textColor}>{arrow.title}
+            </span>
           </div>
+
         </a>
       </div>
     )
--- src/components/arrowPrev.js
+++ src/components/arrowPrev.js
@@ -1,26 +1,24 @@
 import React from "react"
 import { IoIosArrowBack } from "react-icons/io"
-import { IoIosArrowForward } from "react-icons/io/index"
 
 const ArrowPrev = ({ arrow, anchor, fullPageApi }) => {
   if (arrow !== undefined && arrow !== null) {
     return (
-      <div className={"flex items-center customArrow customArrowPrev mt-32 mb-auto md:mt-auto lg:my-auto w-8 h-16 md:w-10 md:h-20 lg:w-24 lg:h-48 " + arrow.backgroundColor}>
-        <a href={"#" + anchor + "/" + arrow.anchor} className={"mx-auto no-underline text-3xl md:text-4xl lg:text-5xl " + arrow.textColor}
+      <div
+        className={"flex items-center customArrow customArrowPrev my-auto w-8 h-40 md:w-10 md:h-20 lg:w-24 lg:h-48 " + arrow.backgroundColor}>
+        <a href={"#" + anchor + "/" + arrow.anchor}
+           className={"h-full w-full no-underline text-3xl md:text-4xl lg:text-5xl " + arrow.textColor}
            onClick={(e) => {
              e.preventDefault()
              console.log(anchor, arrow.anchor)
 
              fullPageApi.moveTo(anchor, arrow.anchor)
            }}>
-          <div className="flex justify-center items-center">
-            <div className="order-2 lg:order-1">
-              <div className="invisible lg:visible relative h-20 w-10 min-w-full">
-                <span
-                  className={"arrowText rotateArrowTextPrev text-sm whitespace-no-wrap uppercase " + arrow.titleColor}>{arrow.title}</span>
-              </div>
-            </div>
-            <span className="order-1 lg:order-2"><IoIosArrowBack/></span>
+          <div className="h-full w-full flex flex-wrap lg:flex-no-wrap justify-around align-center content-center">
+            <span style={{writingMode: "sideways-lr"}}
+                  className={"order-2 lg:order-1 mx-auto my-auto text-sm whitespace-no-wrap uppercase " + arrow.textColor}>{arrow.title}
+            </span>
+            <span className="order-1 lg:order-2 mx-auto my-auto"><IoIosArrowBack/></span>
 
           </div>
         </a>
