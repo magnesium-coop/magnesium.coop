0920f5c1844e551a16e84d91f6b7b874ff4b2a2d
raúl <rsperoni@magnesium.coop>
2020-09-01 12:43:14 -0300

    loader

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,8 @@
 // custom typefaces
 import "./src/css/tailwind.css"
+
+export const onInitialClientRender = () => {
+  setTimeout(function() {
+    document.getElementById("___loader").style.display = "none"
+  }, 1000)
+}
