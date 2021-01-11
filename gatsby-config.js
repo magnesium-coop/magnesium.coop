578354b596ab32bcb25a62d7fda3d5a9a7440ca6
raúl <rsperoni@magnesium.coop>
2021-01-11 16:26:27 -0300

    pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: `/magnesium.coop`,
   siteMetadata: {
     title: `Magnesium.coop`,
     description: `Desarrollamos y diseñamos software, aplicaciones y páginas web para solucionar problemas y mejorar procesos.`,
